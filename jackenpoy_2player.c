0c44157be7ee4d8bff2c51096fba58da21068ec4
rccadungan <rccadungan@student.apc.edu.ph>
2020-01-06 03:18:22 +0000

    Completely Woking Game

--- jackenpoy_2player.c
+++ jackenpoy_2player.c
@@ -19,7 +19,7 @@ curs_set(FALSE);
 
 mvaddstr (1, 80, "Welcome!\n");
 mvaddstr (2, 65, "This is a rock, paper, and scissors game.\n");
-mvaddstr (3, 30, "To play the game, enter 1 for Rock, 2 for Paper and 3 for Scissors. This is a two-player game. Press ENTER to see the result.");
+mvaddstr (3, 25, "To play the game, enter 1 for Rock, 2 for Paper and 3 for Scissors. This is a two-player game. Press ENTER to see the result.");
 mvaddstr (6, 45, "Player 1 Choice: ");
 player1_input = getch();
  	if (player1_input == '1') {
@@ -49,34 +49,34 @@ reload();
       
     if ((player1_input=='1')&&(player2_input=='1'))
       {
-        mvaddstr(19, 80,"Nobody Wins");
+        mvaddstr(20, 78,"Nobody Wins");
       }
     else if ((player1_input=='1')&&(player2_input=='2'))
-      { mvaddstr(19, 80,"Paper Covers Rock");
+      { mvaddstr(20, 77,"Paper Covers Rock");
       }
     else if ((player1_input=='1')&&(player2_input=='3'))
-      { mvaddstr(19, 80,"Rock Breaks Scissors");
+      { mvaddstr(20, 75,"Rock Breaks Scissors");
       }
     else if((player1_input=='2')&&(player2_input=='1'))
-      { mvaddstr(19, 80,"Paper Covers Rock");
+      { mvaddstr(20, 77,"Paper Covers Rock");
       }
     else if((player1_input=='2')&&(player2_input=='2'))
-      { mvaddstr(19, 80,"Nobody Wins");
+      { mvaddstr(20, 78,"Nobody Wins");
       }
     else if((player1_input=='2')&&(player2_input=='3'))
-      { mvaddstr(19, 80,"Scissors Cuts Paper");
+      { mvaddstr(20, 75,"Scissors Cuts Paper");
       }
     else if((player1_input=='3')&&(player2_input=='1'))
-      { mvaddstr(19, 80,"Rock Breaks Scissors");
+      { mvaddstr(20, 75,"Rock Breaks Scissors");
       }
     else if((player1_input=='3')&&(player2_input=='2'))
-      { mvaddstr(19, 80,"Scissors Cuts Paper");
+      { mvaddstr(20, 75,"Scissors Cuts Paper");
       }
     else if((player1_input=='3')&&(player2_input=='3'))
-      { mvaddstr(19, 80,"Nobody Wins");
+      { mvaddstr(20, 78,"Nobody Wins");
       }
     else
-      { mvaddstr(19, 80,"You have selected an invalid selection.\n");
+      { mvaddstr(20, 75,"You have selected an invalid selection.\n");
        
       }
 reload();  
@@ -94,47 +94,47 @@ refresh();
 }
 
 int rock (){
-  mvaddstr(10, 45, "    ***      ");
-  mvaddstr(10, 45, "   *****     ");
-  mvaddstr(11, 45, "  *******    ");
-  mvaddstr(12, 45, "   *****     ");
-  mvaddstr(13, 45, "    ***      ");
-  mvaddstr(13, 45, "     *        ");
+  mvaddstr(10, 48, "    ***      ");
+  mvaddstr(10, 48, "   *****     ");
+  mvaddstr(11, 48, "  *******    ");
+  mvaddstr(12, 48, "   *****     ");
+  mvaddstr(13, 48, "    ***      ");
+  mvaddstr(13, 48, "     *        ");
 } 
 
 int paper(){
-  mvaddstr(10, 45, "      ******   ");
-  mvaddstr(10, 45, "     ******    ");
-  mvaddstr(11, 45, "    ******     ");
-  mvaddstr(12, 45, "   ******      ");
-  mvaddstr(13, 45, "  ******       ");
-  mvaddstr(13, 45, " ******        ");
+  mvaddstr(10, 48, "      ******   ");
+  mvaddstr(10, 48, "     ******    ");
+  mvaddstr(11, 48, "    ******     ");
+  mvaddstr(12, 48, "   ******      ");
+  mvaddstr(13, 48, "  ******       ");
+  mvaddstr(13, 48, " ******        ");
 }
 
 int scissors(){
-  mvaddstr(10, 45, "  **    **   ");
-  mvaddstr(10, 45, "   **  **    ");
-  mvaddstr(11, 45, "    ****     ");
-  mvaddstr(12, 45, "    ***      ");
-  mvaddstr(13, 45, "  **   **    ");
+  mvaddstr(10, 48, "  **    **   ");
+  mvaddstr(10, 48, "   **  **    ");
+  mvaddstr(11, 48, "    ****     ");
+  mvaddstr(12, 48, "    ***      ");
+  mvaddstr(13, 48, "  **   **    ");
   mvaddstr(13, 45, "  **   **    ");
 }
 int  rock_p2 (){
-  mvaddstr(10, 110, "    ***      ");
-  mvaddstr(10, 110, "   *****     ");
-  mvaddstr(11, 110, "  *******    ");
-  mvaddstr(12, 110, "   *****     ");
-  mvaddstr(13, 110, "    ***      ");
-  mvaddstr(13, 110, "     *        ");
+  mvaddstr(10, 112, "    ***      ");
+  mvaddstr(10, 112, "   *****     ");
+  mvaddstr(11, 112, "  *******    ");
+  mvaddstr(12, 112, "   *****     ");
+  mvaddstr(13, 112, "    ***      ");
+  mvaddstr(13, 112, "     *        ");
 } 
 
 int paper_p2 (){
-  mvaddstr(10, 110, "      ******   ");
-  mvaddstr(10, 110, "     ******    ");
-  mvaddstr(11, 110, "    ******     ");
-  mvaddstr(12, 110, "   ******      ");
-  mvaddstr(13, 110, "  ******       ");
-  mvaddstr(13, 110, " ******        ");
+  mvaddstr(10, 112, "      ******   ");
+  mvaddstr(10, 112, "     ******    ");
+  mvaddstr(11, 112, "    ******     ");
+  mvaddstr(12, 112, "   ******      ");
+  mvaddstr(13, 112, "  ******       ");
+  mvaddstr(13, 112, " ******        ");
 }
 
 int scissors_p2 (){
